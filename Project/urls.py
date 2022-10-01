2b8e7b9cbfe5eb5033b3f1b114670bffda0323f1
DaminovSherzod <daminovsherzod2002@gmail.com>
2022-10-01 14:30:16 +0500

    plus

--- Project/urls.py
+++ Project/urls.py
@@ -4,12 +4,24 @@ from django.http import JsonResponse
 
 
 def home(request):
-    a = request.GET.get('a')
+    a = request.GET.get('a',0)
     print(a)
-    b = request.GET.get('b')
+    b = request.GET.get('b',0)
     print(b)
-    r = HttpResponse(f'{int(a)+int(b)}')
+    r = HttpResponse(int(a)+int(b))
     return r
+    # if a!=None and b!=None:
+    #     r = HttpResponse(int(a)+int(b))
+    #     return r
+    # elif a==None and b==None:
+    #     r = HttpResponse('0')
+    #     return r
+    # elif a==None:
+    #     r = HttpResponse(b)
+    #     return r
+    # elif b==None:
+    #     r = HttpResponse(a)
+    #     return r
 
 urlpatterns = [
     path('', home),
